17dc76d1de3b5e1b0afd3cb94cb9ac03ed6d3293
Joe Wallace <joewallace@Joes-MacBook-Air.local>
2020-07-10 17:01:46 -0500

    Add gitignore, Procifle and update

--- app.js
+++ app.js
@@ -294,9 +294,10 @@ app.get("/contact", function(req,res){
 
 
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
-});
+
+app.listen(process.env.PORT || 3000,function(){
+    console.log("Server has started Successfully");
+ });
 
 
 
