ac7eac90de4ddc31cc528238b864e6d425b98fae
Joe Wallace <joewallace@Joes-MacBook-Air.local>
2020-07-10 23:52:25 -0500

    make it better

--- app.js
+++ app.js
@@ -245,8 +245,6 @@ app.get("/submit",function (req,res){
         } if (!user){
           
             res.redirect("/register")
-
-
         }
         req.login(user, function(err){
           if (err){
@@ -295,7 +293,7 @@ app.get("/contact", function(req,res){
 
 
 
-app.listen(process.env.PORT || 3000,function(){
+app.listen(3000,function(){
     console.log("Server has started Successfully");
  });
 
